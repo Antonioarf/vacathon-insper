1264e8db09272580c2dcc195ce9b4fed2b50e55f
Antonioarf <49568529+Antonioarf@users.noreply.github.com>
2020-10-29 21:49:55 -0300

    atualizacao pros robotica

--- assets/untitled1.py
+++ assets/untitled1.py
@@ -1,19 +1,43 @@
+import numpy as np
+import scipy
 import matplotlib.pyplot as plt
-import numpy
+import seaborn as sns
+from random import randint
 # creating the dataset 
-data = {'1/19':0.5,'2/19':0.6,'3/19':0.5,'4/19':0.4,'5/19':0.2,'6/19':0.6,'7/19':0.4,'8/19':0.5,'9/19':0.4,'10/19':0.7,'11/19':0.6,'12/19':0.4,} 
-courses = list(data.keys()) 
-values = list(data.values()) 
+# data = {'1/19':0.5,'2/19':0.6,'3/19':0.5,'4/19':0.4,'5/19':0.2,'6/19':0.6,'7/19':0.4,'8/19':0.5,'9/19':0.4,'10/19':0.7,'11/19':0.6,'12/19':0.4,} 
+# courses = list(data.keys()) 
+# values = list(data.values()) 
    
-fig = plt.figure(figsize = (10, 5)) 
+# fig = plt.figure(figsize = (10, 5)) 
   
-# creating the bar plot 
-plt.bar(courses, values,  
-        width = 0.4) 
+# # creating the bar plot 
+# plt.bar(courses, values,  
+#         width = 0.4) 
+data = []
+for e in range(360):
+    data.append(randint(10,20)/100)
+# for e in range(90):
+#     data.append(100+(randint(0,10)))
+# for e in range(90):
+#     data.append(100+(randint(5,15)))
+# for e in range(90):
+#     data.append(100+(randint(10,20)))
+    
+data = np.array(data)
+norm_cdf = scipy.stats.norm.cdf(data)
+# data = [100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4),100+ randint(-2,4)]
+data2 = range(len(data))
+# plt.plot(data2,data,'o')
+# sns.lineplot(x=data2, y=norm_cdf)
 # plt.grid(True)
-plt.xlabel("Mês") 
-# plt.ylabel("% ") 
-plt.title("Vendas")
-plt.savefig('g2.png')
+# x1,x2,y1,y2 = plt.axis()
+# plt.axis((0,30,0,1))
+
+plt.bar(data2, data,width = 0.4)
+plt.xlabel("Data") 
+plt.ylabel("% dos custos") 
+plt.title("Historico de Vendas")
+plt.savefig('g22.png')
 #plt.show()
+
 
